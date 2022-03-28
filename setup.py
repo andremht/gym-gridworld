f57b4e643133aee14f19a80767568557cdc27b5a
David Widmann <david.widmann@it.uu.se>
2022-03-28 21:35:35 +0200

    Add arguments to `setup`

--- setup.py
+++ setup.py
@@ -2,5 +2,9 @@ from setuptools import setup
 
 setup(name='gym_gridworld',
       version='0.0.1',
+      author='André Teixeira',
+      description='A variation of the OpenAI gym Frozen Lake, mimicking the minimalistic GridWorld',
+      url='https://github.com/andremht/gym_gridworld',
+      packages=setuptools.find_packages(),
       install_requires=['gym>=0.2.3']
 )

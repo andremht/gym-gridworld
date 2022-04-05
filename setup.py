f46fbc77200bf0f80f110854bc2c6be3b6d3f3b8
andremht <43183372+andremht@users.noreply.github.com>
2022-04-05 15:21:09 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,6 +3,11 @@ from setuptools import setup
 setup(name='gym_gridworld',
       version='0.0.1',
       author='André Teixeira',
+      classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+      ],
       description='A variation of the OpenAI gym Frozen Lake, mimicking the minimalistic GridWorld',
       url='https://github.com/andremht/gym_gridworld',
       packages=setuptools.find_packages(),
